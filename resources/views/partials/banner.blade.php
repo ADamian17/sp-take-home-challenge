a11294bd18165cd695b737f7b2b02a469cf63fe0
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-03 12:49:07 -0400

    fixed card filter

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,17 +1,27 @@
  <!-- second banner -->
-<div class="row mt-5 bg-white g-0">
-  <div class="col">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#a1a2a7"></rect></svg>
+<div class="my-banner mt-5">
+  <!-- right side -->
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#a1a2a7"></rect></svg>
+
+  <div class="my-banner__left">
+
+    <div>
+      <p>
+        <strong>Join 100,000+people</strong> 
+        and get resources</p>
+        <p>that have helped thousands of your</p>
+        <p>peers get better at their jobs.</p>
+  
+      <form class="mt-3">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Enter your email" aria-label="Enter your email">
+          <button class="btn btn-danger shadow-sm">
+            Join Now
+          </button>
+        </div>
+      </form>
+    </div>
+
   </div>
-  <div class="col p-5">
-    <p><strong>Join 100,000+ people</strong> and get resources that have helped thousands of your peers get better at their jobs.</p>
-    <form>
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Enter your email" aria-label="Enter your email">
-        <button class="btn btn-danger shadow-sm">
-          Join Now
-        </button>
-      </div>
-    </form>
-  </div>  
+
 </div>
